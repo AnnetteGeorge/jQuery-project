62a01b0eceb8b38c18a6684e2e0e74cb796f6d94
Annette <annettejestine@icloud.com>
2025-01-02 10:01:33 -0800

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 $("h1").addClass("big-title");
 $("button").addClass("button-color");
-$("a").attr("href");
+$("a").attr("href","https://www.udemy.com");
+$("h1").click(function(){
+    $("h1").css("color","red");
+});
+$("button").click(function(){
+    $("h1").css("color","yellow");
+});
+$("button").click(function(){
+    $("h1").text("welcome");
+});
+$("button").on("click",function(){
+    $("h1").slideUp().slideDown().animate({opacity:0.5});
+});
